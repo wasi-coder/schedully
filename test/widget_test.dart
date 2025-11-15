4246571d01d33ab123b75641c980363191e5e67f
WASI AL SAFA <2103058@student.ruet.ac.bd>
2025-11-16 01:20:00 +0600

    3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:schedully/main.dart';
+import 'package:schedully/pages/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
